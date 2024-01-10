1d5cd27d93434f361ca2fe0b9a19fa342b8e57c8
Pablo <pabloat@iescastelar.com>
2024-01-10 12:30:09 +0100

    servicios

--- resources/views/servicios/index.blade.php
+++ resources/views/servicios/index.blade.php
@@ -4,16 +4,20 @@
     <div class="container mx-auto mt-8">
         <h1 class="text-4xl font-bold text-center mb-8">Tarifa del Servicio</h1>
 
-        <div class="bg-white p-8 rounded-lg shadow-md max-w-md mx-auto">
-            @foreach($servicios as $i => $servicio)
-
-                <div class="mb-6">
-                    <img src="{{ asset('servicio_images/servicio_' . $i . '.png') }}" class="w-full h-64 object-cover rounded">
-                    <h2 class="text-2xl font-semibold mb-2">{{ $servicio->nombre }}</h2>
-                    <p class="text-gray-600">Precio: ${{ $servicio->precio }}</p>
-                </div>
-                <hr class="border-t my-4">
-            @endforeach
+        <div class="bg-white p-8 rounded-lg shadow-md max-w-5xl mx-auto">
+            <div class="flex flex-wrap justify-center -mx-4">
+                @foreach($servicios as $i => $servicio)
+                    <div class="w-full md:w-1/2 lg:w-1/5 px-4 mb-6">
+                        <div class="flex items-center">
+                            <img src="{{ asset('servicio_images/servicio_' . ($i + 1) . '.png') }}" class="w-full h-30 object-cover rounded">
+                        </div>
+                        <div class="mt-4">
+                            <h2 class="text-lg font-semibold mb-2 text-center">{{ $servicio->nombre }}</h2>
+                            <p class="text-gray-600 text-center">Precio: ${{ $servicio->precio }}</p>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
