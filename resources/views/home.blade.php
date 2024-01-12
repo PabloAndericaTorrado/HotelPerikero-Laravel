f2b7af578c5bfea20bb6de9817f65918fce67cfb
Pablo <pabloat@iescastelar.com>
2024-01-12 09:54:47 +0100

    Boton cerrrar sesssssiion NAZI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,11 @@
             @endif
 
             <p>{{ __('You are logged in!') }}</p>
+
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit" class="mt-4 bg-red-500 text-white px-6 py-3 rounded-md hover:bg-red-700 transition duration-300 ease-in-out">Logout</button>
+            </form>
         </div>
     </div>
 @endsection
-
