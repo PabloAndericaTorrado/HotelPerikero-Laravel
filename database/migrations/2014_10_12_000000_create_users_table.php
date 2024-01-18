678eaf270296677745d8475eb348ee0c2d00df8f
Pablo <pabloat@iescastelar.com>
2024-01-18 12:36:37 +0100

    añadido mas campos a usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('telefono')->nullable();
+            $table->string('direccion')->nullable();
+            $table->string('provincia')->nullable();
+            $table->string('codigo_postal')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
