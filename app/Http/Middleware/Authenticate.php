aedfb3fedd3ac25d730f349b04d0d1bb9dc5b222
manuxpr <manuelnb@iescastelar.com>
2024-01-18 12:35:45 +0100

    Entrar sin registrarte

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Mockery\Matcher\Closure;
 
 class Authenticate extends Middleware
 {
