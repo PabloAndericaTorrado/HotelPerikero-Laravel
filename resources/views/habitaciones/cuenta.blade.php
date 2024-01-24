71a78b6422851b92d0d65117008b096cfeb6268c
FernandoBaqueroo <fernandobz@iescastelar.com>
2024-01-24 10:31:14 +0100

    Terminando vista de perfil de reservas

--- resources/views/habitaciones/cuenta.blade.php
+++ resources/views/habitaciones/cuenta.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto mt-8">
-        <div class="max-w-3xl mx-auto bg-white p-8 rounded shadow-lg">
+        <div class="max-w-4xl mx-auto bg-white p-8 rounded shadow-lg">
             <h1 class="text-4xl font-semibold mb-8">Mi Perfil</h1>
 
             <div class="mb-4">
@@ -31,7 +31,9 @@
                                     <span class="mr-4" id="{{ $campo.'Usuario' }}">{{ auth()->user()->$campo }}</span>
                                 </div>
                                 @if ($label !== 'Nombre' && $label !== 'Apellidos')
-                                    <button onclick="editarCampo('{{ $campo.'Usuario' }}', '{{ $campo }}')" class="text-blue-500">Editar</button>
+                                    <button onclick="editarCampo('{{ $campo.'Usuario' }}', '{{ $campo }}')"
+                                            class="text-blue-500">Editar
+                                    </button>
                                 @endif
                             </div>
                         @endforeach
@@ -53,7 +55,9 @@
                                 <div class="flex items-center">
                                     <span class="mr-4" id="{{ $campo.'Usuario' }}">{{ auth()->user()->$campo }}</span>
                                 </div>
-                                <button onclick="editarCampo('{{ $campo.'Usuario' }}', '{{ $campo }}')" class="text-blue-500">Editar</button>
+                                <button onclick="editarCampo('{{ $campo.'Usuario' }}', '{{ $campo }}')"
+                                        class="text-blue-500">Editar
+                                </button>
                             </div>
                         @endforeach
                     </div>
@@ -66,48 +70,90 @@
                     <!-- Estructura de reservas -->
                     <h2 class="text-2xl font-semibold mb-4">Reservas</h2>
                     @if(count(auth()->user()->reservas) > 0)
-                        <ul class="space-y-4">
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
                             @foreach(auth()->user()->reservas as $reserva)
-                                <li class="border p-4 rounded">
-                                    <div class="flex justify-between items-center">
-                                        <p class="text-lg font-semibold">{{ $reserva->habitacion->tipo }} - Habitación {{ $reserva->habitacion->numero }}</p>
-                                        <p class="text-gray-600">{{ $reserva->created_at->diffForHumans() }}</p>
-                                    </div>
-                                    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2">
-                                        <div>
-                                            <p><strong>Check-in:</strong> {{ $reserva->check_in }}</p>
-                                            <p><strong>Check-out:</strong> {{ $reserva->check_out }}</p>
-                                        </div>
-                                        <div>
-                                            <p><strong>Precio Noche:</strong> ${{ $reserva->habitacion->precio }}</p>
-                                            <p><strong>Precio Total:</strong> ${{ number_format($reserva->calculateTotalPrice(), 2) }}</p>
-                                            <p><strong>Noches:</strong> {{ $reserva->calculateTotalDays() }} Noches</p>
-                                            <p><strong>Pagado:</strong> {{ $reserva->pagado ? 'Sí' : 'No' }}</p>
-                                            <form action="{{ route('reservas.delete.view', $reserva->id) }}" method="get">
-                                                @csrf
-                                                <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-600 active:bg-red-700 focus:outline-none focus:border-red-800 focus:ring focus:ring-red-300 disabled:opacity-25 transition">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 mr-2">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                                                    </svg>
-                                                    Cancelar Reserva
-                                                </button>
-                                            </form>
+                                <div class="border p-4 rounded-lg shadow-md max-w-xl mx-auto mb-6 bg-white">
+                                    <div class="relative mb-4">
+                                        @php
+                                            $imagenPath = 'habitacion_images/habitacion_' . $reserva->habitacion->id . '.jpg';
+                                        @endphp
+                                        @if(file_exists(public_path($imagenPath)))
+                                            <img src="{{ asset($imagenPath) }}" alt="Imagen de la habitación"
+                                                 class="w-full h-auto rounded-lg mb-4">
+                                        @else
+                                            <p class="text-gray-600 text-center p-4">No hay imagen disponible para esta
+                                                habitación.</p>
+                                        @endif
+                                        <div class="absolute top-0 right-0 p-2 bg-blue-500 text-white rounded-bl-lg">
+                                            <p class="font-semibold">{{ $reserva->habitacion->tipo }}</p>
+                                            <p>Habitación {{ $reserva->habitacion->numero }}</p>
                                         </div>
                                     </div>
-                                </li>
+
+                                    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+                                        <div class="flex justify-center items-center border-b pb-4">
+                                            <p><strong>Entrada:</strong> {{ $reserva->check_in }}</p>
+                                        </div>
+                                        <div class="flex justify-center items-center border-b pb-4">
+                                            <p><strong>Salida:</strong> {{ $reserva->check_out }}</p>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="bg-gray-100 p-4 rounded w-full gap-4 mb-4">
+                                        <p><strong>Alojamiento:</strong></p>
+                                        <p><strong>Gastos de gestión:</strong> $0.00</p>
+                                        <p><strong>Precio por noche</strong> {{  $reserva->habitacion->precio  }}</p>
+                                        <p><strong>Estancia total:</strong> {{ $reserva -> calculateTotalDays() }} </p>
+                                        <p><strong>Pagado:</strong> {{ $reserva->pagado ? 'Sí' : 'No' }}</p>
+
+                                        <div class="my-3">
+                                            <hr class="border-t-2 border-gray-300">
+                                        </div>
+
+                                        <p class="text-center"><strong>Total:</strong> ${{ number_format((float)$reserva->calculateTotalPrice(), 2) }}</p>
+                                    </div>
+
+
+
+                                    <div class="text-center">
+                                        <form action="{{ route('reservas.delete.view', $reserva->id) }}" method="get"
+                                              class="mt-4">
+                                            @csrf
+                                            <button type="submit"
+                                                    class="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-full inline-flex items-center transition-all duration-300">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                     stroke="currentColor" class="h-4 w-4 mr-2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                          stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                                                </svg>
+                                                Cancelar Reserva
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
                             @endforeach
-                        </ul>
+                        </div>
+
                     @else
                         <p class="text-gray-600">No tienes reservas.</p>
                     @endif
                 </div>
             </div>
+
         </div>
     </div>
 
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <style>
+        .linea-bottom {
+            border-bottom: 1px solid #e5e7eb;
+            padding-bottom: 8px;
+            margin-bottom: 8px;
+        }
+    </style>
     <script>
         function mostrarSeccion(seccionId) {
             document.getElementById('perfilContainer').style.display = 'none';
