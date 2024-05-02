707682d376c068678ad993182f816644e7a978e5
manuxpr <manuelnb@iescastelar.com>
2024-05-02 13:15:50 +0200

    Calendario de reservas terminado

--- resources/views/workers/parking.blade.php
+++ resources/views/workers/parking.blade.php
@@ -3,25 +3,24 @@
 @section('content')
     <div class="container mx-auto px-4">
         <div class="flex flex-col md:flex-row gap-4">
-            <div class="w-full md:w-1/4 bg-gray-900 rounded-lg p-4 h-screen flex flex-col justify-between">
+            <div class="md:w-1/4 bg-gray-900 rounded-lg p-4 h-screen flex flex-col justify-between">
                 <ul class="list-none">
-                    <li><a class="text-white nav-link font-bold text-lg"
-                           href="{{ route('movimientos') }}">Movimientos</a></li>
-                    <li><a class="text-white nav-link font-bold text-lg" href="{{ route('parking_day') }}">Ver
-                            Parking</a></li>
-                </ul>
-            </div>
+                    <li><a class="text-white nav-link font-bold text-lg" href="{{ route('movimientos') }}">Movimientos</a></li>
+                    <li><a class="text-white nav-link font-bold text-lg" href="{{ route('parking_day') }}">Ver Parking</a></li>
+                </ul>            </div>
 
-            <div class="w-full md:w-1/2 bg-gray-900 rounded-lg p-4">
+            <div class="md:flex-1 bg-gray-900 rounded-lg p-4">
                 <div id="scheduler"></div>
-
             </div>
 
-            <div class="w-full md:w-1/4 bg-gray-900 rounded-lg p-4">
-                <!-- Tercer cajón con campo para cambiar las tarifas del parking -->
+            <div class="md:w-1/4 bg-gray-900 rounded-lg p-4">
+
             </div>
         </div>
     </div>
+
+
+
 
     <link rel="stylesheet" href="https://cdn.syncfusion.com/ej2/material.css">
     <script src="https://cdn.syncfusion.com/ej2/dist/ej2.min.js"></script>
